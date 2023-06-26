dc1c8b12122e4da336e95f478421382f8c3904cb
Andy Gallagher <fredex42@gmail.com>
2023-06-26 16:12:41 +0100

    trouble with making stages work

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,5 +3,12 @@ import { App } from "aws-cdk-lib";
 import { ConciergeGraphql } from "../lib/concierge-graphql";
 
 const app = new App();
-new ConciergeGraphql(app, "ConciergeGraphql-PROD", { stack: "content-api", stage: "PROD" });
-new ConciergeGraphql(app, "ConciergeGraphql-CODE", { stack: "content-api", stage: "CODE" });
+new ConciergeGraphql(app, "ConciergeGraphql-PROD-AARDVARK", { stack: "content-api", stage: "PROD-AARDVARK" });
+new ConciergeGraphql(app, "ConciergeGraphql-PROD-AARDVARK", { stack: "content-api-preview", stage: "PROD-AARDVARK" });
+new ConciergeGraphql(app, "ConciergeGraphql-CODE-AARDVARK", { stack: "content-api", stage: "CODE-AARDVARK" });
+new ConciergeGraphql(app, "ConciergeGraphql-CODE-AARDVARK", { stack: "content-api-preview", stage: "CODE-AARDVARK" });
+
+new ConciergeGraphql(app, "ConciergeGraphql-PROD-ZEBRA", { stack: "content-api", stage: "PROD-ZEBRA" });
+new ConciergeGraphql(app, "ConciergeGraphql-PROD-ZEBRA", { stack: "content-api-preview", stage: "PROD-ZEBRA" });
+new ConciergeGraphql(app, "ConciergeGraphql-CODE-ZEBRA", { stack: "content-api", stage: "CODE-ZEBRA" });
+new ConciergeGraphql(app, "ConciergeGraphql-CODE-ZEBRA", { stack: "content-api-preview", stage: "CODE-ZEBRA" });
