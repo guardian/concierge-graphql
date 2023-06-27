33a3b61c80c7991d78304761fdc896da9ad47fa1
Andy Gallagher <fredex42@gmail.com>
2023-06-27 16:07:33 +0100

    switch target DNS domain

--- cdk/lib/hostname.ts
+++ cdk/lib/hostname.ts
@@ -3,18 +3,18 @@ import {GuStack} from "@guardian/cdk/lib/constructs/core";
 export function getHostName(scope:GuStack):string {
     if(scope.stage.startsWith("CODE")) {
         if(scope.stack.endsWith("preview")) {
-            return "concierge-graphql-preview.capi.code.dev-gutools.co.uk";
+            return "concierge-graphql-preview.content.code.dev-guardianapis.com";
         } else {
-            return "concierge-graphql.capi.code.dev-gutools.co.uk";
+            return "concierge-graphql.content.code.dev-guardianapis.com";
         }
     } else if(scope.stage.startsWith("PROD")) {
         if (scope.stack.endsWith("preview")) {
-            return "concierge-graphql-preview.capi.gutools.co.uk";
+            return "concierge-graphql-preview.content.guardianapis.com";
         } else {
-            return "concierge-graphql.capi.gutools.co.uk";
+            return "concierge-graphql.content.guardianapis.com";
         }
     } else if(scope.stage=="TEST") {    //CI testing
-        return "concierge-graphql-preview.capi.code.dev-gutools.co.uk";
+        return "concierge-graphql-preview.content.code.dev-guardianapis.com";
     } else {
         throw "stage must be either CODE or PROD";
     }
