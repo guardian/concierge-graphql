065ae08e6fbb53a22ed4829a32f348f2aefdd11f
Andy Gallagher <fredex42@gmail.com>
2024-03-15 16:47:42 +0000

    Try to add usageplan back in

--- cdk/lib/gateway.ts
+++ cdk/lib/gateway.ts
@@ -52,21 +52,22 @@ export class HttpGateway extends Construct {
             createDefaultStage: true,
         });
 
-        // const plan = new CfnUsagePlan(this, "GQLUsagePlan", {
-        //     apiStages: [
-        //         {
-        //             apiId: httpApi.apiId,
-        //             stage: httpApi.defaultStage?.stageName,
-        //             throttle: {
-        //                 "$default": {
-        //                     burstLimit: 150,
-        //                     rateLimit: 50
-        //                 }
-        //             }
-        //         }
-        //     ],
-        //     description: "Usage plan for access to concierge-graphql"
-        // });
+        const plan = new CfnUsagePlan(this, "GQLUsagePlan", {
+            apiStages: [
+                {
+                    apiId: httpApi.apiId,
+                    stage: httpApi.defaultStage?.stageName,
+                    throttle: {
+                        "$default": {
+                            burstLimit: 150,
+                            rateLimit: 50
+                        }
+                    }
+                }
+            ],
+            description: "Usage plan for access to concierge-graphql"
+        });
+        plan.node.addDependency(httpApi);
 
     }
 }
